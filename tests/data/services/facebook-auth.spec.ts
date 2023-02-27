cc0eef8bc1a155f8cec21d02b2ed6e78c3d30f00
Pedro <pedroaugustoribasp@gmail.com>
2023-02-27 19:19:01 -0300

    :test_tube: Change SUT creation

--- tests/data/services/facebook-auth.spec.ts
+++ tests/data/services/facebook-auth.spec.ts
@@ -3,27 +3,18 @@ import { FacebookAuthenticationService } from '@/data/services'
 import { mock, type MockProxy } from 'jest-mock-extended'
 import { type LoadFacebookUserApi } from '../contracts/apis'
 
-type SutType = {
-  sut: FacebookAuthenticationService
-  loadFacebookUserApi: MockProxy<LoadFacebookUserApi>
-}
-const makeSut = (): SutType => {
-  const loadFacebookUserApi = mock<LoadFacebookUserApi>()
-  const sut = new FacebookAuthenticationService(loadFacebookUserApi)
-  return {
-    sut,
-    loadFacebookUserApi
-  }
-}
-
 describe('Tests Facebook auth', () => {
+  let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>
+  let sut: FacebookAuthenticationService
+  beforeEach(() => {
+    loadFacebookUserApi = mock()
+    sut = new FacebookAuthenticationService(loadFacebookUserApi)
+  })
   it('test LoadFacebookUserApi', async () => {
-    const { sut, loadFacebookUserApi } = makeSut()
     await sut.perform({ token: '1234' })
     expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: '1234' })
   })
   it('test return error', async () => {
-    const { sut, loadFacebookUserApi } = makeSut()
     loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
 
     const authResult = await sut.perform({ token: '1234' })
