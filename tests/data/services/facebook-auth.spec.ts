e722dcbb55917eb32f9384dc496e336ced5cf7a8
Pedro <pedroaugustoribasp@gmail.com>
2023-02-27 19:08:17 -0300

    :clown_face: Add new mock

--- tests/data/services/facebook-auth.spec.ts
+++ tests/data/services/facebook-auth.spec.ts
@@ -1,20 +1,17 @@
 import { AuthError } from '@/domain/errors'
 import { FacebookAuthenticationService } from '@/data/services'
+import { mock } from 'jest-mock-extended'
+import { type LoadFacebookUserApi } from '../contracts/apis'
 
 describe('Tests Facebook auth', () => {
   it('test LoadFacebookUserApi', async () => {
-    const loadFacebookUserApi = {
-      loadUser: jest.fn()
-    }
-
+    const loadFacebookUserApi = mock<LoadFacebookUserApi>()
     const sut = new FacebookAuthenticationService(loadFacebookUserApi)
     await sut.perform({ token: '1234' })
     expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: '1234' })
   })
   it('test return error', async () => {
-    const loadFacebookUserApi = {
-      loadUser: jest.fn()
-    }
+    const loadFacebookUserApi = mock<LoadFacebookUserApi>()
     loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
     const sut = new FacebookAuthenticationService(loadFacebookUserApi)
     const authResult = await sut.perform({ token: '1234' })
